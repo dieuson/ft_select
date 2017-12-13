d971b5c168caad876d903e964a0088a87281ec0f
Dieuson VIRGILE <dvirgile@e2r9p18.42.fr>
2017-12-13 14:03:52 +0100

    rename files

--- srcs/build_files_list.c
+++ srcs/build_files_list.c
@@ -5,8 +5,6 @@ t_var 					*create_var_cell(char *name)
 	FT_INIT(t_var*, cell, NULL);
 	if (!(cell = (t_var*)malloc(sizeof(t_var))))
 		return (var_error("malloc", "create_cell"));
-	if (ft_strrchr(name, '/') && (int)ft_strlen(ft_strrchr(name, '/') + 1))
-		name = ft_strrchr(name, '/') + 1;
 	cell->name = ft_strdup(name);
 	cell->disp_attribute = T_NORMAL;
 	cell->index = 0;
